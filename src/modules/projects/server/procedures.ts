56de22d7d5d84d817d560871a4693b95bfa14c06
jishkhar <jishnukhargharia11@gmail.com>
2025-07-21 13:38:22 +0530

    Add Message UI

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -1,20 +1,27 @@
 import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { tr } from "date-fns/locale";
 import z from "zod";
 import { generateSlug } from "random-word-slugs";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
-    getMany: baseProcedure
-        .query(async () => {
-            const projects = await prisma.project.findMany({
-                orderBy: {
-                    updatedAt: "desc",
+    getOne: baseProcedure
+        .input(z.object({
+            id: z.string().min(1, { message: "Id is required." }),
+        }))
+        .query(async ({ input }) => {
+            const existingProject = await prisma.project.findUnique({
+                where: {
+                    id: input.id,
                 },
             });
 
-            return projects;
+            if(!existingProject) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Project not found." });
+            }
+
+            return existingProject;
         }),
 
     create: baseProcedure
