a82152a71e0db9f6b6daa07b4349a44831c35994
jishkhar <jishnukhargharia11@gmail.com>
2025-07-22 22:44:39 +0530

    working till now

---
+++ src/components/user-control.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
+
+interface Props {
+    showName?: boolean;
+}
+
+export const UserControl = ({ showName }: Props) => {
+    return (
+        <UserButton 
+            showName={showName}
+            appearance={{
+                elements: {
+                    userButtonBox: "rounded-md!",
+                    userButtonAvatarBox: "rounded-md! size-8!",
+                    userButtonTrigger: "rounded-md!",
+                },
+            }}
+        />
+    );
+};
